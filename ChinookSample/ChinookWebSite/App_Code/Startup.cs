3821abe236e1da5e42787a8b108b4a0d6ca0516a
dkingma1 <dkingma1@studentmail.nait.ca>
2016-09-26 15:37:16 -0600

    Create website for ChinookSample

---
+++ ChinookSample/ChinookWebSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChinookWebSite.Startup))]
+namespace ChinookWebSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
