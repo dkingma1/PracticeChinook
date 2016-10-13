b0af354c5d57f29afedc926e3fe9bd7ad9970882
dkingma1 <dkingma1@nait.ca>
2016-10-13 17:00:40 -0600

    Setup of startup role for the application
    SecurityRoles file and the load method insdie the RoleManager

--- ChinookSample/ChinookSystem/Security/RoleManager.cs
+++ ChinookSample/ChinookSystem/Security/RoleManager.cs
@@ -16,6 +16,21 @@ namespace ChinookSystem.Security
         public RoleManager() : base(new RoleStore<IdentityRole>(new ApplicationDbContext()))
         {
             
-        }  
+        } 
+        
+        //This method will be executed when the application starts up under IIS(internet information services)
+        public void AddStartupRoles()
+        {
+            foreach(string roleName in SecurityRoles.StartupSecurityRoles)
+            {
+                //Check if the role already exists in the security tables located in the database
+                if(!Roles.Any(r => r.Name.Equals(roleName)))
+                {
+                    //role is not currently on the database
+                    this.Create(new IdentityRole(roleName));
+                }
+            }
+        }
+         
     } 
 }
